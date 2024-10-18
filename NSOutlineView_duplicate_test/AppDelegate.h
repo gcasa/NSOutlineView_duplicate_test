5e95d4fc6813493d9a111d547e6edb1544038ee9
Gregory Casamento <greg.casamento@gmail.com>
2024-10-17 23:39:09 -0400

    Initial Commit

---
+++ NSOutlineView_duplicate_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSOutlineView_duplicate_test
+//
+//  Created by Gregory John Casamento on 10/17/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSOutlineView_duplicate_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSOutlineView_duplicate_test
+//
+//  Created by Gregory John Casamento on 10/17/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
