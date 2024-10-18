a8f66a185a84efe52fdf8d4334eeaf8865b16c3c
Gregory Casamento <greg.casamento@gmail.com>
2024-10-18 00:01:28 -0400

    Add initial files

--- NSOutlineView_duplicate_test/AppDelegate.m
+++ NSOutlineView_duplicate_test/AppDelegate.m
@@ -8,14 +8,16 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+@property (strong) IBOutlet NSOutlineView *outline;
 @property (strong) IBOutlet NSWindow *window;
+@property (strong) id object;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    self.object = @"SAME STRING";
 }
 
 
@@ -28,5 +30,51 @@
     return YES;
 }
 
+// Outline view data source
+- (NSInteger) outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(nullable id)item
+{
+    if (item == nil)
+        return 3;
+    /*
+    else if ([item isEqualToString: @"Item 0"])
+        return 1;
+    else if ([item isEqualToString: @"Item 1"])
+        return 0;
+    */
+    
+    return 0;
+}
+
+- (id) outlineView:(NSOutlineView *)outlineView
+             child:(NSInteger)index
+            ofItem:(nullable id)item
+{
+    /*
+    if (index == 0 && item == nil)
+    {
+        return @"Item 0";
+    }
+    else if (index == 0 && [item isEqualToString:@"Item 0"])
+    {
+        return @"Item 1";
+    }
+    */
+    
+    return self.object;
+}
+
+- (BOOL) outlineView:(NSOutlineView *)outlineView
+    isItemExpandable:(id)item
+{
+    if (item == nil) return YES;
+    return NO;
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item
+{
+    return item;
+}
+
+// Outline view delegate
 
 @end
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSOutlineView_duplicate_test
